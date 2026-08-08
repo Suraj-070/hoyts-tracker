e5dfe9401957e318338e53bda4f3b7c62ac9392f
Suraj-070 <sooraj.stha070@gmail.com>
2026-08-08 18:18:33 +1000

    fix: add all missing closing divs to balance HallCard

--- app/page.js
+++ app/page.js
@@ -487,6 +487,9 @@ function HallCard({ hallName, hall, expanded, onToggle, delay = 0, cinemaId = "E
             })}
           </div>
         )}
+        </div>
+        </div>
+        </div>
       </div>
     </div>
   )
