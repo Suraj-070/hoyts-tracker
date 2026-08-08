4341aa661b6a66b738ed373d9743ac96faced229
Suraj-070 <sooraj.stha070@gmail.com>
2026-08-08 18:29:27 +1000

    fix: add final 2 closes - balanced

--- app/page.js
+++ app/page.js
@@ -472,6 +472,8 @@ function HallCard({ hallName, hall, expanded, onToggle, delay = 0, cinemaId = "E
         )}
         </div>
         </div>
+        </div>
+        </div>
       </div>
     </div>
   )
