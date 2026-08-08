800497d0a9b0966bd80ca026e6ba57fe3875f1ed
Suraj-070 <sooraj.stha070@gmail.com>
2026-08-08 18:28:54 +1000

    fix: remove em-dash unicode causing parse error, fix div count

--- app/page.js
+++ app/page.js
@@ -470,10 +470,6 @@ function HallCard({ hallName, hall, expanded, onToggle, delay = 0, cinemaId = "E
             })}
           </div>
         )}
-        </div>
-        </div>
-        </div>
-        </div>
       </div>
     </div>
   )
