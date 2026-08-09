9f1065ed18d0dfd187cdefa298aa8d7020807cfd
Suraj-070 <sooraj.stha070@gmail.com>
2026-08-09 16:06:28 +1000

    debug more endpoints

--- app/api/hoyts/debug/route.js
+++ app/api/hoyts/debug/route.js
@@ -1,30 +1,27 @@
 const HOYTS_BASE = 'https://apim-aea.hoyts.com.au/cinemaapi-au-live/api'
+const TMDB_KEY = '26b1201a577ece50ab34775a74fb7d5e'
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url)
   const movieId = searchParams.get('id') || 'HO00011139'
   const headers = { 'User-Agent': 'Mozilla/5.0', 'Accept': 'application/json' }
+  const results = {}
 
-  const paths = [
-    '/film/' + movieId,
-    '/films/' + movieId,
-    '/movies/' + movieId,
-    '/content/movies/' + movieId,
-    '/content/film/' + movieId,
+  // Try HOYTS website directly
+  const webPaths = [
+    'https://www.hoyts.com.au/api/movies/' + movieId,
+    'https://www.hoyts.com.au/api/film/' + movieId,
+    HOYTS_BASE + '/movie/' + movieId,
+    HOYTS_BASE.replace('cinemaapi-au-live', 'contentapi-au-live') + '/film/' + movieId,
+    HOYTS_BASE.replace('cinemaapi-au-live', 'contentapi-au-live') + '/movies/' + movieId,
   ]
 
-  const results = {}
-  for (const path of paths) {
+  for (const path of webPaths) {
     try {
-      const res = await fetch(HOYTS_BASE + path, { headers, cache: 'no-store' })
-      results[path] = { status: res.status, ok: res.ok }
-      if (res.ok) {
-        const d = await res.json()
-        results[path].data = d
-      }
-    } catch (e) {
-      results[path] = { error: e.message }
-    }
+      const res = await fetch(path, { headers, cache: 'no-store' })
+      results[path] = { status: res.status }
+      if (res.ok) results[path].data = await res.json()
+    } catch (e) { results[path] = { error: e.message } }
   }
 
   return Response.json(results, { headers: { 'Access-Control-Allow-Origin': '*' } })
